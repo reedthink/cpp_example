12ede1c98152846a68141f59a4e908a1c900532d
jkjk <reedthink@qq.com>
2024-05-26 16:21:22 +0800

    添加匿名函数示例

---
+++ lambda.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+// 同时用函数和lambda实现a+b
+
+int add(int a, int b)
+{
+    return a + b;
+}
+
+int main()
+{
+    int x, y;
+    x = 10;
+    y = 20;
+    int z = 100;
+    std::cout << add(x, y) << std::endl;
+
+    // 全部捕获，不加参数
+    auto addByLambda = [&] {
+        return x + y;
+    };
+
+    // 捕获指定的参数，加参数
+    auto add2ByLambda = [x, &y](int a, int b) {
+        std::cout << x + y << std::endl; // 不仅可以用捕获的参数，还能用传入的
+        return a + b;
+    };
+
+    std::cout << addByLambda() << std::endl;
+    std::cout << add2ByLambda(x, z) << std::endl; // 不仅可以用捕获的参数，还能用传入的
+
+    return 0;
+}
